1eb49a9c0d75295c874c634ee856b44f213cc0b3
Evans <okemwa.ramimi@gmail.com>
2023-05-30 12:14:08 +0300

    my timing program

---
+++ Assignement 1.kt
@@ -0,0 +1,22 @@
+fun main(){
+    var time =-8
+
+    if (time >=1 && time <=10){
+        println("GOOD MORNING")
+
+    }else if (time >=11 && time <=13){
+        println("NOON")
+
+    }else if(time >=14 && time <=16){
+        println("GOOD AFTERNOON")
+    }
+    else if (time >=17 && time <=20){
+        println("GOOD EVENING")
+
+    }else if (time >=21 && time <=24){
+        println("GOOD NIGHT")
+
+    }else{
+        print("UNRECOGNIZED TIME")
+    }
+}
